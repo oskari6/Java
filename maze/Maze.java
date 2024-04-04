3fa3e739986b5928deefaf3cc5c1a744d3ac2e4d
oskari6 <oskarisulkakoski@gmail.com>
2024-04-04 09:00:20 +0300

    4/4//24

--- maze/Maze.java
+++ maze/Maze.java
@@ -1,32 +1,63 @@
 import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Hashing.Position;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Maze{
-    static Structure m = new Structure();
     //0 = wall
     //1 = path
     //2 = destination
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
 
-        int[][] maze = {
-            {1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
-            {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
-            {0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1},
-            {1, 1, 1, 2, 0, 1, 0, 1, 0, 1, 0},
-            {0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0},
-            {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1}
-    };
-    m.maze = maze;
-    m.start = new Position(4,8);
-    m.path = new LinkedList<Position>();
+        ArrayList<Structure> mazes = readMazes();
 
-        if(solveMaze(m.start)){
-            System.out.println("You Won");
-        }else{
-            System.out.println("No path");
+        int i = 0;
+        while(i < mazes.size()){
+            if(solveMaze(mazes.get(i))){
+                System.out.println("You Won");
+            }else{
+                System.out.println("No path");
+            }
+            i++;
         }
     }
 
-    private static boolean solveMaze(Position p){
+    private static ArrayList<Structure> readMazes() throws FileNotFoundException {
+
+        ArrayList<Structure> mazes = new ArrayList<Structure>();
+
+        Scanner in = new Scanner(new File("mazes.txt"));
+
+        while(in.hasNext()) {
+            //fill list from file
+            Structure m = new Structure();
+
+            int rows = Integer.parseInt(in.nextLine());
+            m.maze = new int[rows][];
+    
+            for(int i = 0; i < rows; i++){
+            String line = in.nextLine();
+            m.maze[i] = Arrays.stream(line.split(", ")).mapToInt(Integer::parseInt).toArray();
+            }
+    
+            m.start = new Position(Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine()));
+    
+            in.nextLine(); //get rid off hyphen
+
+            mazes.add(m);
+        }
+        in.close();
+
+        return mazes;
+    }
+    private static boolean solveMaze(Structure m){
+
+        Position p = m.start;
         m.path.push(p);
 
         while(true){
@@ -36,7 +67,7 @@ public class Maze{
             m.maze[y][x] = 0;
 
             //down
-            if(isValid(y+1,x)){
+            if(isValid(y+1,x, m)){
                 if(m.maze[y+1][x] == 2){
                     System.out.println("Moved down");
                     return true;
@@ -48,7 +79,7 @@ public class Maze{
             }
 
             //left
-            if(isValid(y,x-1)){
+            if(isValid(y,x-1, m)){
                 if(m.maze[y][x-1] == 2){
                     System.out.println("Moved left");
                     return true;
@@ -60,7 +91,7 @@ public class Maze{
             }
 
             //up
-            if(isValid(y-1,x)){
+            if(isValid(y-1,x, m)){
                 if(m.maze[y-1][x] == 2){
                     System.out.println("Moved up");
                     return true;
@@ -72,7 +103,7 @@ public class Maze{
             }
 
             //right
-            if(isValid(y,x+1)){
+            if(isValid(y,x+1, m)){
                 if(m.maze[y][x+1] == 2){
                     System.out.println("Moved right");
                     return true;
@@ -91,7 +122,7 @@ public class Maze{
         }
     }
 
-    public static boolean isValid(int y, int x){
+    public static boolean isValid(int y, int x, Structure m){
         if(y < 0 || y >= m.maze.length ||
            x < 0 || x >= m.maze[y].length){
            return false; 
--- maze/Structure.java
+++ maze/Structure.java
@@ -1,7 +1,9 @@
 import java.util.LinkedList;
 
+import Hashing.Position;
+
 public class Structure {
     public int[][] maze;
-    public LinkedList<Position> path;
+    public LinkedList<Position> path = new LinkedList<Position>();
     public Position start;
 }
